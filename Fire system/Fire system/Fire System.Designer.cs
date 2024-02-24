0e7243fba0ae7992a95b2bf93c5cbd4eec22d11d
HuSSam_Mislmani <73494744+HuSSam-lab@users.noreply.github.com>
2024-02-24 19:20:26 +0400

    Add files via upload

---
+++ Fire system/Fire system/Fire System.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Fire_system
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(241, 673);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkGray;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1253, 68);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(0, 68);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1253, 605);
+            this.panel3.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 50);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 50);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(25, 400);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(185, 50);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(25, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start system";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_3);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1253, 673);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Segoe UI Symbol", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Fire System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ Fire system/Fire system/Fire System.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Fire_system
+{
+    public partial class Form1 : Form
+    {
+        System.Random random = new System.Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool r2Stat, Firelook2, gasSensor2, door2stat, win2stat,
+             r1Stat, Firelook1, gasSensor1, firesensor1, door1stat, win1stat,
+             primaryTank, EmergencyTank;
+        bool onoff = false;
+        
+        bool firesensor2 = false;
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!door2stat)
+            {
+                door2stat = true;
+                doorstat2.BackColor = Color.Green;
+
+                doorstat2.Refresh();
+                Thread.Sleep(2000);
+
+                door2stat = false;
+                doorstat2.BackColor = Color.Red;
+            }
+        }
+
+        private void EbuttonR1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void FireLook2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        public bool rand(Label x) 
+        {
+            
+
+          //  Random _random = new Random();
+
+            if (((int)random.Next(0, 20) / 10) == 1)
+            {
+                x.BackColor = System.Drawing.Color.Green;
+
+                return true;
+            }
+            else {
+                x.BackColor = System.Drawing.Color.Red;
+                return false; }
+            
+        }
+
+
+
+        private void scuduling() { 
+
+            //turn on fire look 2
+        Firelook2 = true; 
+        FireLook2.BackColor = Color.Green;
+
+            // generate state for gas sensor2 after some time 
+        FireLook2.Refresh();
+        Thread.Sleep(2000);        
+        gasSensor2 = rand(GasSensor2);
+
+            // gas sensor or fire look have a proplem
+        if (!gasSensor2)
+        {
+            sys2stat.Text = "Error occured";
+            sys2stat.BackColor = Color.Coral;
+        }
+
+
+            
+        else {
+            sys2stat.Text = "system is stable";
+            sys2stat.BackColor = Color.AliceBlue;
+        }
+
+            // check if primary tank is empty
+        if (!primaryTank) {
+            TankStat.ForeColor = Color.Red;
+            TankStat.Text = "Primary Tank is Empty"; 
+            
+        
+        
+        }
+
+             // turn off fire look after scuduling
+        Thread.Sleep(2000);
+        Firelook2 = false;
+        FireLook2.BackColor = Color.Red;
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //fire sensor stat genrator 
+            onoff = !onoff;
+            
+            //to calculate system stat
+            bool sysstat = !(firesensor2 | firesensor1);
+
+            if (onoff)
+            { //to generate tank status
+                primaryTank = rand(FullTankP);
+
+                if (primaryTank)
+                {
+                    FullTankP.Text = "100%";
+                    FullTankP.BackColor = Color.Transparent;
+                    FullTankP.ForeColor = Color.Green;
+                    EmptyTankP.Text = "";
+                }
+                else
+                {
+                    FullTankP.Text = "";
+                    EmptyTankP.Text = "Empty";
+                    EmptyTankP.BackColor = Color.Red;
+                }
+            }
+            
+ //           EmergencyTank = rand(FullTankE);
+   //         if (EmergencyTank)
+     //           FullTankE.Text = "100";
+
+
+
+
+
+            if (sysstat)
+            {
+                sysStat.ForeColor = Color.Green;
+                sysStat.Text = "All Clear";
+            }
+            else {
+                sysStat.ForeColor = Color.Red;
+                sysStat.Text = "Alert!!";
+            }
+            if (onoff)
+            {
+                SystemOnOff.BackColor = Color.Green;
+                if (firesensor2)
+                {
+                    win2stat = false;
+                    winStat2.BackColor = Color.Red;
+                    Firelook2 = true;
+                    FireLook2.BackColor = Color.Green;
+                    win1stat = rand(winStat1);
+                    door1stat = rand(doorstat1);
+                    door2stat = false;
+                    doorstat2.BackColor = Color.Red;
+
+                    return;
+                }
+                else if (firesensor1)
+                {
+                    FireSensor1_Click(sender, e);
+                    win2stat = rand(winStat2);
+                    door2stat = rand(doorstat2);
+                    door1stat = rand(doorstat1);
+                    return;
+                }
+                else 
+                {
+                    win2stat = rand(winStat2);
+                    door2stat = rand(doorstat2);
+                    door1stat = rand(doorstat1);
+                    win1stat = rand(winStat1);
+                }
+            }
+            else 
+            {
+                Firelook2 = false;
+                FireLook2.BackColor = Color.Red;
+                SystemOnOff.BackColor = Color.Red; 
+            }
+            
+        }
+
+
+
+        //if (firesensor2) { 
+            //Firelook2 = true;
+            //win2stat = false;
+            //Thread.Sleep(5000);
+            //door2stat = false;
+
+            //}
+          //  else Firelook2 = false;
+
+
+
+        private void Systat2_Click(object sender, EventArgs e)
+        {
+
+            if (Firelook2 && !gasSensor2) 
+            {
+                r2Stat = false;
+                Systat2.BackColor = Color.Red;
+            }
+        }
+
+        private void FireSensor2_Click(object sender, EventArgs e)
+        {
+
+            if (onoff)
+            {
+                firesensor2 = !firesensor2;
+                if (firesensor2)
+                {   
+                    FireSensor2.BackColor = Color.Red;
+
+                    sys2stat.ForeColor = Color.Red;
+                    sys2stat.Text = "room on fire       ";
+                  
+                    // change All system stutes
+                    sysStat.ForeColor = Color.Red;
+                    sysStat.Text = "room 2 is burning";
+
+                    // turn on fire look
+                    Firelook2 = true;
+                    FireLook2.BackColor = Color.Green;
+
+
+
+                    win2stat = false;
+                    winStat2.BackColor = Color.Red;
+
+                    door2stat = false;
+                    doorstat2.BackColor = Color.Red;
+
+                    Firelook2 = false;
+                    FireLook2.BackColor = Color.Red;
+                    
+                }
+                else
+                {
+                    FireSensor2.BackColor = Color.Green;
+
+                    // change All system stutes
+                    sysStat.BackColor = Color.Green;
+                    sysStat.Text = "All Clear";
+
+                    // change room system stutes
+                    sys2stat.BackColor = Color.Green;
+                    sys2stat.Text = "fire is terminated";
+
+                  
+                    
+                }
+
+
+            }
+
+        }
+
+        private void winStat2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void winStat1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FireSensor1_Click(object sender, EventArgs e)
+        {
+            firesensor1 = true;
+            FireSensor1.BackColor = Color.Red;
+            if (onoff)
+            {
+                
+
+                win1stat = false;
+                winStat1.BackColor = Color.Red;
+
+
+
+
+            }
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (onoff && !firesensor2)
+            {
+                scuduling();
+            }
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_3(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
